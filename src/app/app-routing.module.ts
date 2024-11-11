f20b3c118cf5f278ac22ec664750a3ddacfa2e00
Iustin6x <iustinserian@gmail.com>
2024-11-11 19:28:29 +0200

    Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
-];
+    { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) },
+    { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+    { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+    { path: '**', redirectTo: 'tasks' }
+  ];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
